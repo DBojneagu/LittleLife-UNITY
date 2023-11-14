dabc2f2b046e43f0b616f3d3a00095555f2c846f
Talpiga Andrei-Vlad <vlad.talpiga@gmail.com>
2023-11-14 16:15:53 +0200

    appear-dissappear-diamonds

--- Assets/Scripts/CoinCollision.cs
+++ Assets/Scripts/CoinCollision.cs
@@ -1,35 +1,43 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody))]
 public class CoinCollision : MonoBehaviour
 {
 
     private Rigidbody _rigidbody;
-    private float i = 0.0f;
-    public GameObject coin;
+    private float _respawnTime = 3.0f;
+    private int _countCoins = 0;
+    //public Text coinText;
 
     private void Awake() =>
         _rigidbody = GetComponent<Rigidbody>();
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        for (var i = 0; i < collision.contactCount; i++)
-            if (collision.contacts[i].otherCollider.CompareTag("Coin"))
-                Debug.Log("Coin!");
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
-            Debug.Log("Reappearing Coin!");
-            Destroy(other.gameObject);
-            if (Time.time > i)
-            {
-                i += 2;
-                Instantiate(other.gameObject);
-            }
-        }//instantiate, destroy= enable, disable
+            // Increment coin count
+            _countCoins++;
+            Debug.Log(_countCoins);
+            //coinText.text = "Coins: " + _countCoins.ToString(); 
+
+            // Disable the coin
+            other.gameObject.SetActive(false);
+
+            // Start a coroutine to wait for 3 seconds and then enable the coin
+            StartCoroutine(EnableCoinAfterDelay(_respawnTime, other));
+        }
+    }
+
+    private IEnumerator EnableCoinAfterDelay(float delay, Collider other)
+    {
+        // Wait for the specified time
+        yield return new WaitForSeconds(delay);
+
+        // Enable the coin and make it visible
+        other.gameObject.SetActive(true);
     }
 
 }
---
+++ Assets/SimpleCollectiblesPack/SimpleCollectibleScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SimpleCollectibleScript : MonoBehaviour {
+
+	public enum CollectibleTypes {NoType, Type1, Type2, Type3, Type4, Type5}; // you can replace this with your own labels for the types of collectibles in your game!
+
+	public CollectibleTypes CollectibleType; // this gameObject's type
+
+	public bool rotate; // do you want it to rotate?
+
+	public float rotationSpeed;
+
+	public AudioClip collectSound;
+
+	public GameObject collectEffect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (rotate)
+			transform.Rotate (Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			Collect ();
+		}
+	}
+
+	public void Collect()
+	{
+		if(collectSound)
+			AudioSource.PlayClipAtPoint(collectSound, transform.position);
+		if(collectEffect)
+			Instantiate(collectEffect, transform.position, Quaternion.identity);
+
+		//Below is space to add in your code for what happens based on the collectible type
+
+		if (CollectibleType == CollectibleTypes.NoType) {
+
+			//Add in code here;
+
+			Debug.Log ("Do NoType Command");
+		}
+		if (CollectibleType == CollectibleTypes.Type1) {
+
+			//Add in code here;
+
+			Debug.Log ("Do NoType Command");
+		}
+		if (CollectibleType == CollectibleTypes.Type2) {
+
+			//Add in code here;
+
+			Debug.Log ("Do NoType Command");
+		}
+		if (CollectibleType == CollectibleTypes.Type3) {
+
+			//Add in code here;
+
+			Debug.Log ("Do NoType Command");
+		}
+		if (CollectibleType == CollectibleTypes.Type4) {
+
+			//Add in code here;
+
+			Debug.Log ("Do NoType Command");
+		}
+		if (CollectibleType == CollectibleTypes.Type5) {
+
+			//Add in code here;
+
+			Debug.Log ("Do NoType Command");
+		}
+
+		Destroy (gameObject);
+	}
+}
