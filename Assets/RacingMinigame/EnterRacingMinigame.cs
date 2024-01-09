a318c766f3813263e54fa7a5619d11011db0c479
Vlad Talpiga <vlad.talpiga@gmail.com>
2024-01-09 15:13:08 +0200

    racing mingame + pudu move fix

--- Assets/RacingMinigame/EnterRacingMinigame.cs
+++ Assets/RacingMinigame/EnterRacingMinigame.cs
@@ -4,17 +4,16 @@ using UnityEngine.SceneManagement;
 public class EnterRacingMinigame : MonoBehaviour
 {
     public string sceneToLoad;
-    public PlayerPositionHandler playerPositionHandler;
+    //public PlayerPositionHandler playerPositionHandler;
     public Transform playerTransform;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ferrari"))
         {
-            PlayerPrefs.Save();
             // Save the player's position before transitioning to another scene
-            playerPositionHandler.SavePlayerPosition();
-            PlayerPrefs.SetFloat("PlayerX", playerTransform.position.x);
-            PlayerPrefs.SetFloat("PlayerY", playerTransform.position.y);
+            //playerPositionHandler.SavePlayerPosition();
+            PlayerPrefs.SetFloat("PlayerX", playerTransform.position.x - 5);
+            PlayerPrefs.SetFloat("PlayerY", playerTransform.position.y - 5);
             PlayerPrefs.SetFloat("PlayerZ", playerTransform.position.z);
             PlayerPrefs.Save();
             SceneManager.LoadScene(sceneToLoad);
