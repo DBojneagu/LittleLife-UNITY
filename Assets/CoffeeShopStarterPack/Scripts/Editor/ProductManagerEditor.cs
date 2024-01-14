a3c25455e8e0333da8c2c873beb6c185f6cae35e
Mara Neagu <marateodoraneagu@gmail.com>
2024-01-14 20:03:36 +0200

    json for the character changes

--- Assets/CoffeeShopStarterPack/Scripts/Editor/ProductManagerEditor.cs
+++ Assets/CoffeeShopStarterPack/Scripts/Editor/ProductManagerEditor.cs
@@ -1,12 +1,4 @@
-﻿// ******------------------------------------------------------******
-// ProductManagerEditor.cs
-//
-// Author:
-//       K.Sinan Acar <ksa@puzzledwizard.com>
-//
-// Copyright (c) 2019 PuzzledWizard
-//
-// ******------------------------------------------------------******
+﻿
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
