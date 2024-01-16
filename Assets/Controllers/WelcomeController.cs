3634d887a3ee7717afdf8f30613336700506d7c7
Vlad Talpiga <vlad.talpiga@gmail.com>
2024-01-16 10:57:18 +0200

    build added

--- Assets/Controllers/WelcomeController.cs
+++ Assets/Controllers/WelcomeController.cs
@@ -1,12 +1,36 @@
 using System.IO;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WelcomeController : MonoBehaviour
 {
-    public void NewGame()
+    public Button sceneChangeButton;
+
+    public string sceneToLoad;
+
+    void Start()
+    {
+        Debug.Log("starttttt");
+        sceneChangeButton.onClick.AddListener(ChangeScene);
+    }
+
+    void ChangeScene()
+    {
+        Debug.Log("xxxxxxx");
+        string userPath = Application.dataPath + "/Data/UserData.json";
+
+        UserData user = new UserData();
+        string json = JsonUtility.ToJson(user, true);
+        File.WriteAllText(userPath, json);
+        PlayerPrefs.SetFloat("PlayerX", 16f);
+        PlayerPrefs.SetFloat("PlayerY", 0f);
+        PlayerPrefs.SetFloat("PlayerZ", -9f);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(sceneToLoad);
+    }
+/*    public void NewGame()
     {
         string userPath = Application.dataPath + "/Data/UserData.json";
 
@@ -17,6 +41,6 @@ public class WelcomeController : MonoBehaviour
         PlayerPrefs.SetFloat("PlayerY", 0f);
         PlayerPrefs.SetFloat("PlayerZ",-9f);
         PlayerPrefs.Save();
-        SceneManager.LoadScene("SampleScene");
-    }
+        // SceneManager.LoadScene("SampleScene");
+    }*/
 }
